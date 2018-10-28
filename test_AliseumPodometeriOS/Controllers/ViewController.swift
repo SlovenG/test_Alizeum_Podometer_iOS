3525163f783f265987cc4c68fd50914d33059fdc
Sloven Graciet <sloven@yogowo.com>
2018-10-28 19:40:38 +0100

    correct animation different Device

--- test_AliseumPodometeriOS/Controllers/ViewController.swift
+++ test_AliseumPodometeriOS/Controllers/ViewController.swift
@@ -14,11 +14,16 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
+    var positionAnim: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        self.setOffsetPositionAnim()
+        
         let animationView = LOTAnimationView(name: "deadPoolGray")
-        animationView.frame = CGRect(x: self.view.frame.maxX, y: self.titleLabel.frame.minY - 75, width: 250, height:  250)
+        animationView.frame = CGRect(x: self.view.frame.maxX, y: positionAnim.y, width: 250, height:  250)
         animationView.contentMode = .scaleAspectFill
         animationView.animationSpeed = 1
         animationView.loopAnimation = true
@@ -28,13 +33,26 @@ class ViewController: UIViewController {
         animationView.play()
         
         UIView.animate(withDuration: 3, animations: {
-             animationView.frame = CGRect(x:120, y: self.titleLabel.frame.minY - 75 , width: 250, height: 250)
+             animationView.frame = CGRect(x:self.positionAnim.x, y: self.positionAnim.y , width: 250, height: 250)
         }) { (finished) in
             DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
                 self.performSegue(withIdentifier: "dataStepsSegue", sender: self)
             }
         }
         
+    }
+    
+    func setOffsetPositionAnim(){
+        switch UIDevice.current.screenType {
+        case .iPhone_XR,.iPhone_XSMax,.iPhones_X_XS:
+            self.positionAnim = CGPoint(x: 120, y: 210)
+        case .iPhones_6_6s_7_8:
+            self.positionAnim = CGPoint(x: 100, y: 90)
+        case .iPhones_6Plus_6sPlus_7Plus_8Plus:
+            self.positionAnim = CGPoint(x: 120, y: 125)
+        default:
+            self.positionAnim = CGPoint(x: 100, y: 50)
+        }
     }
 
 }
